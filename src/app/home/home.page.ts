f27629d2b42320106efb301829ffdb9b6435a5e0
shailesh <shailesh.jain@vernost.in>
2020-06-03 21:40:38 +0530

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { GetDataService } from '../services/get-data.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+  providers: [GetDataService]
 })
 export class HomePage implements OnInit {
 
   homeText;
-  constructor() { }
+  constructor(private getDataService: GetDataService) { }
 
   ngOnInit() {
-    this.homeText = "This is home content";
+    this.homeText = [];
     console.log("home text", this.homeText);
+    let data = (this.getDataService.getData());
+    data.div.forEach((d) => {
+      d.lines.forEach((l) => {
+        this.homeText.push(l['punjabi-text']);
+      });
+    });
+    console.log("data.heading == ", data.heading);
   }
 
 }
--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { GetDataService } from '../services/get-data.service'
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
+  providers: [GetDataService]
 })
 export class SearchPage implements OnInit {
 
-  constructor() { }
+  homeText;
+  constructor(private getDataService: GetDataService) { }
 
   ngOnInit() {
+    this.homeText = [];
+    console.log("home text", this.homeText);
+    let data = (this.getDataService.getData());
+    data.div.forEach((d) => {
+      d.lines.forEach((l) => {
+        this.homeText.push(l['punjabi-text']);
+      });
+    });
+    console.log("data.heading == ", data.heading);
   }
 
 }
