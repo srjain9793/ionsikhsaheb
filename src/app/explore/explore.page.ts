aaae1e55580c3d9957fdfa82fe7c6646cafd7087
shailesh <shailesh.jain@vernost.in>
2020-06-14 20:52:33 +0530

    explore

--- src/app/explore/explore.page.ts
+++ src/app/explore/explore.page.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExplorePage implements OnInit {
 
+  exploreList: any;
   constructor() { }
 
   ngOnInit() {
+    this.exploreList = [
+      { label: "Chapter Index", value: "" },
+      { label: "Ang By Ang", value: "" },
+      { label: "Shabad index", value: "" },
+      { label: "Gurdas Var Index", value: "" },
+      { label: "Sri Dasam Granth Index", value: "" },
+      { label: "Sri Dasam Granth Page by Page", value: "" },
+      { label: "Kabit by Kabit", value: "" },
+      { label: "Bhai Nand lal Bani", value: "" },
+
+    ]
   }
+
+
+
 
 }
